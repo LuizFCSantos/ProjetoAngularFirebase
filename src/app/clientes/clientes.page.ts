014dc41f5c249688f17bfce32ade04a3672df575
LuizFCSantos <luiz.scampos17@gmail.com>
2023-10-11 11:57:38 -0300

    endpoints final

--- src/app/clientes/clientes.page.ts
+++ src/app/clientes/clientes.page.ts
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesPage {
  
+  isModalOpen = false;
+
   constructor() {
     this.getFuncionarios();
   }
 
   isLoading: boolean = false;
   funcionarios: any;
+  inserir: any;
+  // atualizar: any;
 
   getFuncionarios(){
     this.isLoading = true;
@@ -51,6 +55,83 @@ export class ClientesPage {
       this.isLoading = false;
     })
   }
+  
+  setInserir(){
+    this.isLoading = true;
+	
+		let inserir = { CodFun: '', 
+                    Sobrenome: '',
+                    Nome: '',
+                    Cargo: '',
+                    DataNasc: '',
+                    Endereco: '',
+                    Cidade: '',
+                    CEP: '',
+                    Pais: '',
+                    Fone: '',
+                    Salario: '' };
+
+    fetch('http://localhost/API_Atividade/funcionarios/inserir_funcionario.php',
+			{
+			  method: 'POST',
+			  headers: {
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify(inserir)
+			}
+		)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      this.inserir = response.inserir
+      console.log(this.inserir)
+    })
+    .catch(erro => {
+      console.log(erro);
+    })
+    .finally(()=>{
+      this.isLoading = false;
+      this.getFuncionarios()
+    })
+  }
+  // getAtualizar(){
+  //   this.isLoading = true;
+	
+	// 	let atualizar = { CodFun: '', 
+  //                   Sobrenome: '',
+  //                   Nome: '',
+  //                   Cargo: '',
+  //                   DataNasc: '',
+  //                   Endereco: '',
+  //                   Cidade: '',
+  //                   CEP: '',
+  //                   Pais: '',
+  //                   Fone: '',
+  //                   Salario: '' };
+
+  //   fetch('http://localhost/API_Atividade/funcionarios/atualizar_funcionario.php',
+	// 		{
+	// 		  method: 'POST',
+	// 		  headers: {
+	// 		    'Content-Type': 'application/json',
+	// 		  },
+	// 		  body: JSON.stringify(atualizar)
+	// 		}
+	// 	)
+  //   .then(response => response.json())
+  //   .then(response => {
+  //     console.log(response);
+  //     this.atualizar = response.atualizar
+  //     console.log(this.atualizar)
+  //   })
+  //   .catch(erro => {
+  //     console.log(erro);
+  //   })
+  //   .finally(()=>{
+  //     this.isLoading = false;
+  //     this.getFuncionarios()
+  //   })
+  // }
 
   
   remover(id:any){
@@ -119,6 +200,15 @@ export class ClientesPage {
     }
     fetch('http://localhost/API_Atividade/funcionarios/consulta_func_nome.php' + url);
   }
+  
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  setOpenModal(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 }
+
 
 
